14532f0bc3852e8e2d769a7987fe1682e312a9ac
shravanbarhanpurkar <87528024+shravanbarhanpurkar@users.noreply.github.com>
2021-08-14 13:21:29 +0530

    Add files via upload

---
+++ sports website/styleIpResults.cs
@@ -0,0 +1,70 @@
+#navbar ul {
+  
+  display: flex;
+  align-content: center;
+ justify-content: center;
+}
+#navbar ul li {
+  padding: 20px;
+  list-style: none;
+}
+#navbar ul li a {
+  text-transform: uppercase;
+  font-weight: bold;
+  color: #fff;
+  font-size: 2em;
+  text-decoration: none;
+  padding: 1em;
+  white-space: nowrap;
+  letter-spacing: 1px;
+  
+}
+#navbar ul li a:hover  {
+  border-bottom: 3px solid #fff;
+  
+}
+
+
+.header {
+  margin: 0;
+  font-weight: 900;
+  font-size: 1rem;
+  text-align: center;
+  color: #fff;
+  margin-bottom: 20px;
+}
+
+
+body {
+    background-color: #000000;
+    background-image: url("images.jpeg")no-repeat  center;
+    font: normal 16px/1.5 "Helvetica Neue", sans-serif;
+    color: #fff;
+    overflow-x: hidden;
+    padding-bottom: 50px;
+} 
+
+.intro {
+  padding: 100px 0;
+}
+
+.container {
+  width: 90%;
+  max-width: 1200px;
+  margin: 0 auto;
+  text-align: center;
+}
+
+body {
+  padding: 20px;
+  height: 100vh;
+  background: url("images.jpeg") no-repeat  center;
+  background-size: cover;
+  position: relative
+}
+#datavalue {
+  display: flex;
+  align-content: center;
+ justify-content: center;
+ size: 20px;
+}
